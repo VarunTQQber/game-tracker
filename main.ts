6505345c4d1ede132887606b7b1cf20548f8ca3f
VarunTQQber <122548038+VarunTQQber@users.noreply.github.com>
2023-01-30 11:23:43 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,44 @@
 function scoreboard () {
     OLED.clear()
-    OLED.writeStringNewLine("Player 1 Score:" + PA)
+    OLED.writeStringNewLine("Player 1 Score: " + PA)
     OLED.newLine()
-    OLED.writeStringNewLine("Player 2 Score:" + PB)
+    OLED.writeStringNewLine("Player 2 Score: " + PB)
     OLED.newLine()
-    OLED.writeStringNewLine("Ties:" + Ties)
+    OLED.writeStringNewLine("Ties: " + Ties)
     OLED.newLine()
-    OLED.writeStringNewLine("Rounds:" + Rounds)
+    OLED.writeStringNewLine("Rounds: " + Rounds)
 }
+input.onButtonPressed(Button.A, function () {
+    basic.showString("A")
+    PA += 1
+    Rounds += 1
+    basic.clearScreen()
+    scoreboard()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("T")
+    Ties += 1
+    Rounds += 1
+    basic.clearScreen()
+    scoreboard()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("B")
+    PB += 1
+    Rounds += 1
+    basic.clearScreen()
+    scoreboard()
+})
+input.onGesture(Gesture.Shake, function () {
+    OLED.clear()
+    PA = 0
+    PB = 0
+    Ties = 0
+    Rounds = 0
+    OLED.writeStringNewLine("Let's play a game!")
+    basic.pause(2000)
+    scoreboard()
+})
 let Rounds = 0
 let Ties = 0
 let PB = 0
